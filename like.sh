bdae7a5b7fe6a4d8cc0f4adce8c0083b5be0c1d2
Simone Bosotti <simoubuntu@users.noreply.github.com>
2020-11-13 20:48:20 +0100

    Uniformed like.sh

--- like.sh
+++ like.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #REQUIRES playlist-modify-public and playlist-modify-private
 
-miotoken=$(curl -s -H "Authorization: Basic base64code" -d grant_type=refresh_token -d refresh_token=refreshtoken https://accounts.spotify.com/api/token | jq -r '.access_token')
+basecode=$(cat /home/pi/apiremote/basecode.tk)
+refresh=$(cat /home/pi/apiremote/refresh.tk)
 
-track=$(curl -X "GET" "https://api.spotify.com/v1/me/player" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $miotoken"| jq -r '.item.uri')
+mytoken=$(curl -s -H "Authorization: Basic $basecode" -d grant_type=refresh_token -d refresh_token=$refresh https://accounts.spotify.com/api/token | jq -r '.access_token')
 
-curl -X "POST" "https://api.spotify.com/v1/playlists/playlistURI/tracks?uris=$track" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $miotoken"
+track=$(curl -X "GET" "https://api.spotify.com/v1/me/player" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $mytoken"| jq -r '.item.uri')
+
+echo "Spotify says $track"
+
+curl -X "POST" "https://api.spotify.com/v1/playlists/1AMZ394W3u1slxX1FYvBZ4/tracks?uris=$track" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $mytoken"
 
 echo "Spotify Like"
